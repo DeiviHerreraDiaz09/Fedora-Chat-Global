bc993deb262d6e77064fbcb777cdac8db41f2415
DeiviHerrera09 <dherrera@grupoasd.com>
2024-05-23 11:13:00 -0500

    FEAT: Chat Global v1

---
+++ main.py
@@ -0,0 +1,56 @@
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from starlette.requests import Request
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: list[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+
+manager = ConnectionManager()
+
+
+@app.get("/")
+async def init(req: Request):
+    return templates.TemplateResponse("index.html", {"request": req})
+
+
+@app.get("/chat")
+async def get(req: Request, username: str):
+    return templates.TemplateResponse(
+        "chat.html", {"request": req, "username": username}
+    )
+
+
+@app.websocket("/ws/{username}")
+async def websocket_endpoint(websocket: WebSocket, username: str):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.broadcast(f"{username}: {data}")
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast(f"{username} se ha retirado del chat")
